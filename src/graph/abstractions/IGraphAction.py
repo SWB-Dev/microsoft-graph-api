1827ede7d5ff3fd484af1a91575a0907eb0134d5
Barnes, Steven <Steven.Barnes@usfoods.com>
2021-10-22 12:42:12 -0700

    refactor IGraphAction to return IGraphResponse

--- src/graph/abstractions/IGraphAction.py
+++ src/graph/abstractions/IGraphAction.py
@@ -1,43 +1,45 @@
 from typing import Protocol, Optional
 
+from ... import IGraphResponse
+
 class IGraphAction(Protocol):
-    def get(self, url:str = None):
+    def get(self, url:str = None) -> IGraphResponse:
         """"""
     
     def get_all(self):
         """"""
 
-    def post(self, url:Optional[str]):
+    def post(self, url:Optional[str]) -> IGraphResponse:
         """"""
     
-    def patch(self, url:Optional[str]):
+    def patch(self, url:Optional[str]) -> IGraphResponse:
         """"""
     
-    def put(self):
+    def put(self) -> IGraphResponse:
         """"""
     
-    def delete(self, url:Optional[str]):
+    def delete(self, url:Optional[str]) -> IGraphResponse:
         """"""   
     
     def build_url(self) -> str:
         """"""
 
 class IGraphPostAction(Protocol):
-    def post(self, url:str = None):
+    def post(self, url:str = None) -> IGraphResponse:
         """"""
 
 class IGraphGetAction(Protocol):
-    def get(self, url:str = None):
+    def get(self, url:str = None) -> IGraphResponse:
         """"""
 
 class IGraphPatchAction(Protocol):
-    def patch(self, url:Optional[str]):
+    def patch(self, url:Optional[str]) -> IGraphResponse:
         """"""
 
 class IGraphPutAction(Protocol):
-    def put(self):
+    def put(self) -> IGraphResponse:
         """"""
 
 class IGraphDeleteAction(Protocol):
-    def delete(self, url:Optional[str]):
+    def delete(self, url:Optional[str]) -> IGraphResponse:
         """"""  
