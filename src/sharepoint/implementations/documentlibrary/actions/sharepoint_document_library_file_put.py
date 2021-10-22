3a8733f166b9a54993fc911abcd32d6b68479b3f
Barnes, Steven <Steven.Barnes@usfoods.com>
2021-10-22 14:04:08 -0700

    add sharepoint implementation: library file put

---
+++ src/sharepoint/implementations/documentlibrary/actions/sharepoint_document_library_file_put.py
@@ -0,0 +1,25 @@
+import io
+import requests
+
+from ..... import IGraphResponse, SharepointGraphClientBase, GraphResponseBase
+
+class SharepointDocumentLibraryFilePUT:
+
+    def __init__(self, request_uri:str, stream:io.BufferedIOBase, client:SharepointGraphClientBase):
+        self.request_uri = request_uri
+        self.stream = stream
+        self.client = client
+        self.graph_request = GraphResponseBase()
+        self.client.add_request(self.graph_request)
+
+    def put(self) -> IGraphResponse:
+        self.client.conn.establish_connection()
+        request_url = f"{self.client.GRAPH_BASE_URI}{self.request_uri}"
+        print(request_url)
+        headers = self.client.conn.headers
+        headers['content-type'] = 'text/plain'
+
+        r = requests.put(request_url, headers=headers, data=self.stream)
+
+        self.graph_request.add_response(r)
+        return self.graph_request
