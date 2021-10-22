1ac83dddfb775cd14fddddd8cf53ff9ee0add1a5
Barnes, Steven <Steven.Barnes@usfoods.com>
2021-10-22 16:48:06 -0700

    add package setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="microsoft-graph-api",
+    version="0.1.0",
+    description="API for Microsoft Graph using refresh tokens.",
+    url="https://github.com/SWB-Dev/microsoft-graph-api/tree/develop",
+    author="Steven Barnes",
+    author_email="steven.barnes@swbdevelopment.com",
+    license="MIT",
+    packages=["src"],
+    install_requires=["requests","json","dataclasses","abc"])
