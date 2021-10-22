712240c001cbbdcaf71be57772c76761cd8f7cbf
Barnes, Steven <Steven.Barnes@usfoods.com>
2021-10-22 11:49:57 -0700

    add graph utility: response base

---
+++ src/graph/utilities/graph_response_base.py
@@ -0,0 +1,20 @@
+from requests.models import Response
+
+class GraphResponseBase:
+    """Implements the IGraphResponse protocol."""
+    def __init__(self):
+        self.responses:list[Response] = []
+    
+    @property
+    def request(self):
+        return [r.request for r in self.responses]
+    
+    @property
+    def response(self):
+        return self.responses
+    
+    def add_response(self, response:Response):
+        self.responses.append(response)
+    
+    def add_responses(self, responses:list[Response]):
+        self.responses.extend(responses)
