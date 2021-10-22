cf2ed641a8f23f0ed43e1dd01e1459fb3cb2d60f
Barnes, Steven <Steven.Barnes@usfoods.com>
2021-10-22 16:47:37 -0700

    update imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,32 +1,32 @@
-from graph.abstractions.IGraphResponse import IGraphResponse
-from graph.abstractions.IGraphAction import IGraphAction, IGraphDeleteAction, IGraphGetAction, IGraphPatchAction, IGraphPostAction, IGraphPutAction
-from graph.abstractions.IGraphFilter import IGraphFilter
+from .graph.abstractions.IGraphResponse import IGraphResponse
+from .graph.abstractions.IGraphAction import IGraphAction, IGraphDeleteAction, IGraphGetAction, IGraphPatchAction, IGraphPostAction, IGraphPutAction
+from .graph.abstractions.IGraphFilter import IGraphFilter
 
-from graph.implementations.graph_filter import GraphFilter
+from .graph.implementations.graph_filter import GraphFilter
 
-from graph.utilities.graph_response_base import GraphResponseBase
+from .graph.utilities.graph_response_base import GraphResponseBase
 
-from sharepoint.abstractions.ISharepointDocumentFolder import ISharepointDocumentFolder
-from sharepoint.abstractions.ISharepointDocumentLibrary import ISharepointDocumentLibrary
-from sharepoint.abstractions.ISharepointListItem import ISharepointListItem
-from sharepoint.abstractions.ISharepointList import ISharepointList
-from sharepoint.abstractions.ISharepointSite import ISharepointSite
-from sharepoint.abstractions.ISharepointGraphRequest import ISharepointGraphRequest
-from sharepoint.abstractions.ISharepointGraphClient import ISharepointGraphClient
+from .sharepoint.abstractions.ISharepointDocumentFolder import ISharepointDocumentFolder
+from .sharepoint.abstractions.ISharepointDocumentLibrary import ISharepointDocumentLibrary
+from .sharepoint.abstractions.ISharepointListItem import ISharepointListItem
+from .sharepoint.abstractions.ISharepointList import ISharepointList
+from .sharepoint.abstractions.ISharepointSite import ISharepointSite
+from .sharepoint.abstractions.ISharepointGraphRequest import ISharepointGraphRequest
+from .sharepoint.abstractions.ISharepointGraphClient import ISharepointGraphClient
 
-from sharepoint.utilities.sharepoint_connection import SharepointConnection
-from sharepoint.utilities.sharepoint_graph_client_base import SharepointGraphClientBase
+from .sharepoint.utilities.sharepoint_connection import SharepointConnection
+from .sharepoint.utilities.sharepoint_graph_client_base import SharepointGraphClientBase
 
-from sharepoint.implementations.listitem.sharepoint_listitem_batch_response import SharepointListItemBatchResponse
-from sharepoint.implementations.listitem.sharepoint_listitem_batch_actions import SharepointListItemBatchAction
-from sharepoint.implementations.listitem.sharepoint_listitem import SharepointListItem
+from .sharepoint.implementations.listitem.sharepoint_listitem_batch_response import SharepointListItemBatchResponse
+from .sharepoint.implementations.listitem.sharepoint_listitem_batch_actions import SharepointListItemBatchAction
+from .sharepoint.implementations.listitem.sharepoint_listitem import SharepointListItem
 
-from sharepoint.implementations.list.sharepoint_list import SharepointList
+from .sharepoint.implementations.list.sharepoint_list import SharepointList
 
-from sharepoint.implementations.documentlibrary.actions.sharepoint_document_library_file_put import SharepointDocumentLibraryFilePUT
+from .sharepoint.implementations.documentlibrary.actions.sharepoint_document_library_file_put import SharepointDocumentLibraryFilePUT
 
-from sharepoint.implementations.documentlibrary.sharepoint_document_library import SharepointDocumentLibrary
+from .sharepoint.implementations.documentlibrary.sharepoint_document_library import SharepointDocumentLibrary
 
-from sharepoint.implementations.site.sharepoint_site import SharepointSite
+from .sharepoint.implementations.site.sharepoint_site import SharepointSite
 
-from sharepoint.utilities.sharepoint_graph_client import SharepointGraphClient
+from .sharepoint.utilities.sharepoint_graph_client import SharepointGraphClient
