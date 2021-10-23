efcc0f8365a14cf17ce08e80b2b4c8d749d08ffa
Barnes, Steven <Steven.Barnes@usfoods.com>
2021-10-22 17:05:37 -0700

    remove unused import

--- msgraph/sharepoint/utilities/sharepoint_graph_client.py
+++ msgraph/sharepoint/utilities/sharepoint_graph_client.py
@@ -1,11 +1,7 @@
-from src.sharepoint.utilities.sharepoint_connection import SharepointConnection
 from ... import IGraphResponse, ISharepointSite
 from ... import SharepointGraphClientBase, SharepointSite
 
 class SharepointGraphClient(SharepointGraphClientBase):
-
-    # def __init__(self, conn:SharepointConnection):
-    #     self.__init_subclass__()
 
     def sites(self, relative_site_uri:str) -> ISharepointSite:
         site = SharepointSite(relative_site_uri, self)
