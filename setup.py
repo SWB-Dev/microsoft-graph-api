19109649bfa809a8d89e9797278c2e2a111ee1b3
Barnes, Steven <Steven.Barnes@usfoods.com>
2021-10-26 13:11:31 -0700

    update url in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="microsoft-graph-api",
     version="1.0.0",
     description="API for Microsoft Graph using refresh tokens.",
-    url="https://github.com/SWB-Dev/microsoft-graph-api/tree/develop",
+    url="https://github.com/SWB-Dev/microsoft-graph-api",
     author="Steven Barnes",
     author_email="steven.barnes@swbdevelopment.com",
     license="MIT",
