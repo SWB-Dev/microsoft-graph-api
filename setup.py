792fdc05a38b5df4e04d7f61df48989d5e284722
Barnes, Steven <Steven.Barnes@usfoods.com>
2021-10-28 15:39:08 -0700

    update setup to traverse package refs

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
+import os
+
+rootabspath = os.path.abspath(__file__)
+rootdir = os.path.join(os.path.dirname(rootabspath))
+ignores = ["__pycache__"]
+packages = []
+
+
+def traverse_package(paths:list):
+    lookingdir = rootdir
+    for p in paths:
+        lookingdir = os.path.join(lookingdir,p)
+    if os.path.isfile(lookingdir):
+        return
+    for dir in os.listdir(lookingdir):
+        if dir not in ignores:
+            next_path = paths.copy()
+            next_path.append(dir)
+            traverse_package(next_path)
+    packages.append('.'.join(paths))
+
+traverse_package(["msgraph"])
+
 from setuptools import setup
 
 setup(
     name="microsoft-graph-api",
-    version="1.0.3",
+    version="1.0.4",
     description="API for Microsoft Graph using refresh tokens.",
     url="https://github.com/SWB-Dev/microsoft-graph-api",
     author="Steven Barnes",
     author_email="steven.barnes@swbdevelopment.com",
     license="MIT",
-    packages=["msgraph","msgraph.graph","msgraph.graph.abstractions","msgraph.graph.implementations","msgraph.graph.utilities","msgraph.sharepoint","msgraph.sharepoint.abstractions","msgraph.sharepoint.implementations","msgraph.sharepoint.utilities"],
+    packages=packages,
     install_requires=["requests"])
